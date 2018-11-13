d9029bec2803fda3c480f3425a1fd20e0403fe1f
Sean Trott <seantrott@wireless-169-228-103-127.ucsd.edu>
2018-11-13 14:05:49 -0800

    Added phonetic analysis

---
+++ src/concreteness_phonetic_onset.py
@@ -0,0 +1,99 @@
+"""Analyze concreteness impact of onsets."""
+
+# Plotting
+import matplotlib.pyplot as plt
+import seaborn as sn
+
+# Operations
+import numpy as np
+import pandas as pd 
+import statsmodels.formula.api as sm
+from tqdm import tqdm
+
+# Custom imports
+from nlp_utilities.compling import CorpusUtilities
+from utils import get_concreteness_for_word
+
+
+# Set global variables
+DATA_PATH = "data/raw/mrc_concreteness.csv"
+CHARACTER_INDEX	= 0
+
+# Get data
+df = pd.read_csv(DATA_PATH, sep="\t")
+df['Word'] = df['Word'].str.lower()
+
+# Rescale concreteness
+df['Concreteness'] = df['Concreteness'] / 100
+
+# Drop duplicates
+df = df.drop_duplicates(subset="Word")
+print("Number of words left in MRC: {num}".format(num=len(df)))
+
+# Merge with celex
+df_celex = pd.read_csv("data/raw/celex_all.csv", sep = "\\")
+df_celex = df_celex.drop_duplicates(subset='Word')
+print("Number of words in CELEX: {num}".format(num=len(df_celex)))
+
+# Merge together to get phonetic representations
+df_merged = df_celex.merge(df, on="Word")
+print("Number of words in both MRC and CELEX: {num}".format(num=len(df_merged)))
+
+# Drop duplicate phonetic representations (homophones)
+df_merged = df_merged.drop_duplicates(subset = 'PhonDISC')
+print("Number of words left: {num}".format(num=len(df_merged)))
+
+# Get minimal sets
+print("Getting minimal sets...")
+minimal_sets = CorpusUtilities.get_minimal_orthographic_sets(df_merged['PhonDISC'], index=CHARACTER_INDEX)
+
+
+
+
+# Calculate onset impact
+onset_to_impact = []
+for set_index, ms in tqdm(list(enumerate(minimal_sets))):
+	letters = [w[CHARACTER_INDEX] for w in ms]
+	by_word_cnc = get_concreteness_for_word(ms, df_merged, column='PhonDISC')
+	mean_cnc = np.mean(by_word_cnc)
+	for index, onset in enumerate(letters):
+		to_add = [onset, by_word_cnc[index] - mean_cnc, mean_cnc, ms[index], set_index]
+		onset_to_impact.append(to_add)
+
+COLUMNS = ['onset', 'concreteness_impact', 'set_mean', 'original_word', 'set_index']
+final_df = pd.DataFrame(onset_to_impact, columns=COLUMNS)
+
+
+
+result = sm.ols(formula="concreteness_impact ~ onset", data=final_df).fit()
+
+
+
+### Shuffling
+shuffled_rsquares = []
+true_rsquare = result.rsquared
+
+
+NUM_SHUFFLES = 1000
+
+for shuffle in tqdm(range(NUM_SHUFFLES)):
+    new_df = final_df.copy()
+    new_df['shuffled_onset'] = np.random.permutation(new_df['onset'])
+    new_result = sm.ols(formula="concreteness_impact ~ shuffled_onset", data=new_df).fit()
+    shuffled_rsquares.append(new_result.rsquared)
+
+
+p_real = len([r for r in shuffled_rsquares if r >= true_rsquare]) / len(shuffled_rsquares)
+print("p={p}".format(p=p_real))
+
+plt.hist(shuffled_rsquares)
+plt.axvline(x=true_rsquare, linestyle="dotted")
+
+
+### vIsualization
+
+
+
+ax = sn.boxplot(data=final_df, x="onset", y="concreteness_impact")
+ax.set(xlabel="Onset phone", ylabel="Concreteness impact")
+ax.axhline(y=0, linestyle="dotted")
--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,9 @@
 """Utility functions."""
 
-def get_concreteness_for_word(words, df):
+def get_concreteness_for_word(words, df, column='word'):
 	"""Look up concreteness of each word in dataframe"""
 	cnc = []
 	for word in words:
-		cnc.append(float(df[df['word'] == word]['Concreteness']))
+		cnc.append(float(df[df[column] == word]['Concreteness']))
 	return cnc
 
